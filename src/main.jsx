eab8228c91f7e996a6b9012bab9b196209e5b7d8
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-02-28 12:39:42 +0100

    set template

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+//? CSS
+import "../src/assets/css/index.css";
+
+//?Bootstrap
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
+import "../node_modules/bootstrap-icons/font/bootstrap-icons.min.css";
+
+//? Components
+import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(
+  // <StrictMode>
+  <App />,
+  // </StrictMode>,
+);
